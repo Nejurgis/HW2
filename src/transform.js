bf9a207471d061d317399708d9b134dbe9dab6f7
quietQuiet <j.lietunovas@gmail.com>
2019-01-19 13:45:59 +0100

    added functionality for 20 and younger

--- src/transform.js
+++ src/transform.js
@@ -1,19 +1,46 @@
-
 function groupAdultsByAgeRange(arr) {
-    if(arr) {
-        arr.reduce((acc, next)=> {
-            console.log('reduce')
-            return {}
-        },{})
-        
-    }  else {
-        console.log('result')
-    }
-   
+
+    return arr.reduce((acc, next) => {
+        if (acc.age > next.age) {
+            return {
+                '20 and younger': acc
+            }
+        } else {
+            return '20 and younger', next
+        }
+    })
+
+
+
     // return groupAdultsByAgeRange
 }
 
 
-console.log (groupAdultsByAgeRange());
+console.log(groupAdultsByAgeRange([{
+    name: "Henry",
+    age: 100
+}, {
+    name: "Edgar",
+    age: 10
+}, {
+    name: "Jonukas",
+    age: 8
+}, {
+    name: "Rein",
+    age: 95
+}]));
 
-module.exports = {groupAdultsByAgeRange}
+module.exports = {
+    groupAdultsByAgeRange
+}
+
+
+// The function will be called with an array of objects as its argument. X
+// The objects represent people, with a property `name` and a property `age`. 
+// Your function should group each person 
+// **over the age of 18** into age ranges. Here are a few examples:
+
+// Example 1
+// groupAdultsByAgeRange([{name: "Henry", age: 9}, {name: "John", age: 20}])
+// Should result in:
+// const result = { '20 and younger': [ { name: 'John', age: 20 } ] }
