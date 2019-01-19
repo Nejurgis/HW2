d491d2dbb768330d71ed3fec05e1c06de3f18d68
quietQuiet <j.lietunovas@gmail.com>
2019-01-19 14:24:19 +0100

    filtered out the ones  < 18, grouping by age

--- src/2-transform.spec.js
+++ src/2-transform.spec.js
@@ -10,14 +10,14 @@ describe("Assignment 2 - transform", function () {
     const grouping = groupAdultsByAgeRange([])
     assert.deepStrictEqual(grouping, {})
   })
-  // it("filters out people under the age of 18", function() {
-  //   const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
-  //   const grouping = groupAdultsByAgeRange([
-  //     { name: "pete", age: 10 },
-  //     { name: "dove", age: 17 },
-  //   ])
-  //   assert.deepStrictEqual(grouping, {})
-  // })
+  it("filters out people under the age of 18", function() {
+    const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
+    const grouping = groupAdultsByAgeRange([
+      { name: "pete", age: 10 },
+      { name: "dove", age: 17 },
+    ])
+    assert.deepStrictEqual(grouping, {})
+  })
   // it("only contains non-empty groups", function() {
   //   const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
   //   const grouping = groupAdultsByAgeRange([{name: "Anna", age: 31}, {name: "John", age: 32}, {name: "Hank", age: 60}])
--- src/transform.js
+++ src/transform.js
@@ -1,13 +1,25 @@
 
 function groupAdultsByAgeRange(arr) {
-
-    return arr.reduce((acc, next)=> {
-        if (acc < next.age ) {
-            return {'20 and younger': acc} 
-        } else {
-            return '20 and younger', next 
+// frst we need to filter and see who is below 18 X
+// Then we need to group by age
+    // we could return an array of ages 
+    
+    return arr.filter(el=> el.age > 18)
+    .reduce((acc,next)=>{
+        if (acc.age > 20) {
+            return {'20 and older': acc}
         }
-    },{})
+        // console.log(acc);
+    })
+
+
+    // return arr.reduce((acc, next)=> {
+    //     if (next.age < 18 ) {
+    //         return 
+    //     } else {
+    //         return 'bigger'
+    //     }
+    // },{})
         
     
    
@@ -15,9 +27,24 @@ function groupAdultsByAgeRange(arr) {
 }
 
 
-console.log(groupAdultsByAgeRange([]));
+console.log(groupAdultsByAgeRange([{ name: "pete", age: 10 },{ name: "jonas", age: 25 },{ name: 'John', age: 40 }]));
 
 module.exports = {groupAdultsByAgeRange}
+
+
+// - `20 and younger`
+//     - `21-30`
+//     - `31-40`
+//     - `41-50`
+//     - `51 and older`
+
+// it("filters out people under the age of 18", function() {
+//     const groupAdultsByAgeRange = require("./transform").groupAdultsByAgeRange
+//     const grouping = groupAdultsByAgeRange([
+//       { name: "pete", age: 10 },
+//       { name: "dove", age: 17 },
+//     ])
+
 
 
 // The function will be called with an array of objects as its argument. X
